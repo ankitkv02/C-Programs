dc840e394caa00e5bffd5774109e89f16c07e774
ankitkv02 <45030480+ankitkv02@users.noreply.github.com>
2019-01-30 23:50:13 +0530

    Update SumLastFirst.c
    A simpler program by including math.h header file.

--- LOOPS/SumLastFirst.c
+++ LOOPS/SumLastFirst.c
@@ -3,9 +3,10 @@
  */
 
  #include<stdio.h>
+ #include<math.h>
  int main()
  {
-     int first,last,n,num;
+    /* int first,last,n,num;
      printf("Enter the number :");
      scanf("%d",&n);
      num=n;
@@ -18,5 +19,22 @@
      }
      printf("\nFirst digit of a number is %d",first);
      printf("\nLast digit of a number is %d",last);
-     return 1;
+     return 1;*/
+  
+     int n,digits,first,last;
+
+     printf("Enter a Number to know its First & Last digits : ");
+     scanf("%d",&n);
+
+     last = n%10;
+
+     digits = (int)log10(n);
+
+     first = (int)(n/pow(10,digits));
+
+     printf("The first digit is %d and the last digit is %d \n",first,last);
+     printf("%d contains %d digits. \n",n,digits+1);  //Number of digits can be printd as well to make output more info rich.
+
+     return 0;
+    
  }
