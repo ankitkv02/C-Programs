408c98c2a3905d08450f075938c60713d0939668
ankitkv02 <45030480+ankitkv02@users.noreply.github.com>
2019-02-01 15:38:48 +0530

    Update Alphabets.c
    Printing Z to A

--- LOOPS/Alphabets.c
+++ LOOPS/Alphabets.c
@@ -5,6 +5,7 @@
  #include<stdio.h>
  int main()
  {
+    
      char ch;
      int i=97;
      printf("\nAlphabets from a - z are :");
@@ -13,5 +14,14 @@
          printf("\n%c",i);
          i++;
      }
+  //Program to print all alpha from z to a
+  
+  int j=122;
+  
+  while(j>=97)
+  {
+   printf("\n%c",j);
+   j++;
+  } 
      return 1;
  }
